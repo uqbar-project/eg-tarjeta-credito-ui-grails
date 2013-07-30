f818b453ab72c4d3349f6898837ab6510f1ba1ea
Fernando Dodino <fernando.dodino@gmail.com>
2013-07-30 11:08:28 +0000

    Agregamos formulario modal para comprar, se capturan las excepciones vía ajax

--- src/groovy/ar/edu/clientes/homes/HomeClientes.groovy
+++ src/groovy/ar/edu/clientes/homes/HomeClientes.groovy
@@ -4,29 +4,40 @@ import ar.edu.clientes.ClientePosta
 import ar.edu.clientes.ClientePromocion
 import ar.edu.clientes.ClienteSafeShop
 import ar.edu.clientes.busqueda.ClienteBusqueda
+import ar.edu.clientes.exceptions.BusinessException
 
 @Singleton
 class HomeClientes {
 
-	def clientes
+	Map clientes
 	
 	HomeClientes() {
 		clientes = generarClientes()
 	}
 
+	def getCliente(idCliente) {
+		def cliente = clientes.get(idCliente)
+		if (!cliente) {
+			throw new BusinessException("El cliente " + idCliente + " no existe en el repositorio")
+		}
+		cliente
+	}
+	
 	def getClientes(ClienteBusqueda clienteBusqueda) {
-		clientes.findAll {
-			cliente -> clienteBusqueda.cumple(cliente)
+		clientes.entrySet().findAll {
+			entry -> def cliente = entry.value
+					 clienteBusqueda.cumple(cliente)
 		}
 	}
 		
 	def generarClientes() {
 		[
-			new ClientePosta("Swiss medical", 1500),
-			new ClienteSafeShop(new ClientePosta("SIEMENS", 0), 500),
-			new ClientePromocion(new ClientePosta("Jodor S.A.", 250)),
-			new ClienteSafeShop(new ClientePosta("Matanga Hermanos", 780), 600),
-			new ClientePosta("Ocho Cientos", 800)
+			1: new ClientePosta("Swiss medical", 1500),
+			2: new ClienteSafeShop(new ClientePosta("SIEMENS", 0), 500),
+			3: new ClientePromocion(new ClientePosta("Jodor S.A.", 250)),
+			4: new ClienteSafeShop(new ClientePosta("Matanga Hermanos", 780), 600),
+			5: new ClientePosta("Ocho Cientos", 800)
 		]
 	}
+	
 }
